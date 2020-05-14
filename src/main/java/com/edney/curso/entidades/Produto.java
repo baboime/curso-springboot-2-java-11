3acf88647853cbce73a6708be4808386586e0f68
Edney Baboim <ednney@gmail.com>
2020-05-14 16:00:19 -0300

    Implementação da associação do Produto com ItemDePedido (one-to-many)

--- src/main/java/com/edney/curso/entidades/Produto.java
+++ src/main/java/com/edney/curso/entidades/Produto.java
@@ -11,7 +11,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_produto")
@@ -30,6 +33,9 @@ public class Produto implements Serializable {
 	@ManyToMany
 	@JoinTable(name = "tb_produto_categoria", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private Set<Categoria> categorias = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemDePedido> itens = new HashSet<>();
 
 	public Produto() {
 	}
@@ -85,6 +91,15 @@ public class Produto implements Serializable {
 
 	public Set<Categoria> getCategorias() {
 		return categorias;
+	}
+	
+	@JsonIgnore
+	public Set<Pedido> getPedidos() {
+		Set<Pedido> set = new HashSet<>();
+		for (ItemDePedido x : itens) {
+			set.add(x.getPedido());
+		}
+		return set;
 	}
 
 	@Override
